83260497fa34dccb45e34e65d9fb500a8aff2857
besari <31652673+besari@users.noreply.github.com>
2017-12-14 09:48:48 +0100

    Drawing the line where the animation will take place

--- FBLiveStreamReaction/ViewController.swift
+++ FBLiveStreamReaction/ViewController.swift
@@ -12,14 +12,29 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        let animationView = AnimationView(frame: view.frame)
+        animationView.backgroundColor = .red
+        
+        //add the animationView on the ViewController
+        view.addSubview(animationView)
     }
 
 
 }
 
+class AnimationView: UIView {
+    
+    override func draw(_ rect: CGRect) {
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: 0, y: 200))
+        let endPoint = CGPoint(x: self.frame.width, y:200)
+        let cp1 = CGPoint(x: 100, y: 200)
+        let cp2 = CGPoint(x: 200, y: 200)
+        path.addCurve(to: endPoint, controlPoint1: cp1, controlPoint2: cp2)
+        path.lineWidth = 4
+       
+        path.stroke()
+        
+    }
+}
